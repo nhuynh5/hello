164a9800ebb924fe13867a342e21fe30311542c8
nhuynh5 <nhuynh5@huskers.unl.edu>
2022-08-26 21:36:10 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Nathan Huynh
+ *         nhuynh5@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("I am Nate!\n");
+
+  return 0;
+}
